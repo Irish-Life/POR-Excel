571984e38579770e6d0785abe87d37084d519def
Jonni <bisslad@gmail.com>
2019-05-17 15:31:01 +0100

    Finalised Versoin 1.0

--- por-mi.vb
+++ por-mi.vb
@@ -1,5 +1,5 @@
 Sub cleanData()
-
+    ' Declare variables
     Dim startRange As Range
     Dim idRange As Range
     Dim sellerCodes As Range
@@ -18,20 +18,25 @@ Sub cleanData()
     Dim resultSegments As Range
     Dim resultEventTotals As Range
 
-    Set startRange = Application.Workbooks("POR - Usage_Events_Table").Worksheets(1).Range("A2:A207")
+
+    ' Initialise variables
+    Set startRange = Application.Workbooks("POR - Usage_Events_Table").Worksheets(1).Range("A2:A316")
 
     Set idRange = Application.Workbooks("user extract").Worksheets(1).Range("A2:A116738")
     Set sellerCodes = Application.Workbooks("user extract").Worksheets(1).Range("B2:B116738")
 
-    Set resultIDs = Application.Workbooks("Point-of-Retirement-Reporting").Worksheets(1).Range("A2:A207")
-    Set resultNames = Application.Workbooks("Point-of-Retirement-Reporting").Worksheets(1).Range("B2:B207")
-    Set resultSellerCodes = Application.Workbooks("Point-of-Retirement-Reporting").Worksheets(1).Range("C2:C207")
-    Set resultEventLabels = Application.Workbooks("Point-of-Retirement-Reporting").Worksheets(1).Range("D2:D207")
-    Set resultSegments = Application.Workbooks("Point-of-Retirement-Reporting").Worksheets(1).Range("E2:E207")
-    Set resultEventTotals = Application.Workbooks("Point-of-Retirement-Reporting").Worksheets(1).Range("E2:E207")
+    Set resultIDs = Application.Workbooks("Point-of-Retirement-Reporting").Worksheets(1).Range("A2:A316")
+    Set resultNames = Application.Workbooks("Point-of-Retirement-Reporting").Worksheets(1).Range("B2:B316")
+    Set resultSellerCodes = Application.Workbooks("Point-of-Retirement-Reporting").Worksheets(1).Range("C2:C316")
+    Set resultEventLabels = Application.Workbooks("Point-of-Retirement-Reporting").Worksheets(1).Range("D2:D316")
+    Set resultSegments = Application.Workbooks("Point-of-Retirement-Reporting").Worksheets(1).Range("E2:E316")
+    Set resultEventTotals = Application.Workbooks("Point-of-Retirement-Reporting").Worksheets(1).Range("F2:F316")
 
+    ' Main loop
     For counter = 1 To startRange.Count
 
+
+        If InStr(startRange(counter, 1), "-") > 0 Then
         ' Parse the initial string
         tmp = parseDataString(startRange(counter, 1).Value, "-")
         eventLabel = Trim(tmp(0))
@@ -53,23 +58,56 @@ Sub cleanData()
             End If
 
         Next cell
-
+        End If
     Next counter
+    ' End main loop
+
+
+    ' Define headings
+    Application.Workbooks("Point-of-Retirement-Reporting").Worksheets(1).Range("A1") = "ID"
+    Application.Workbooks("Point-of-Retirement-Reporting").Worksheets(1).Range("B1") = "Name"
+    Application.Workbooks("Point-of-Retirement-Reporting").Worksheets(1).Range("C1") = "Seller_Code"
+    Application.Workbooks("Point-of-Retirement-Reporting").Worksheets(1).Range("D1") = "Event_Label"
+    Application.Workbooks("Point-of-Retirement-Reporting").Worksheets(1).Range("E1") = "Segment"
+    Application.Workbooks("Point-of-Retirement-Reporting").Worksheets(1).Range("F1") = "Event_Total"
+
+    ' Delete empty rows
+    Application.Workbooks("Point-of-Retirement-Reporting").Worksheets(1).Range("A2:F316").Select
+    Selection.SpecialCells(xlCellTypeBlanks).EntireRow.Delete
 
     Debug.Print "Finished!"
 End Sub
 
 
+'Sub sortResults()
+
+ '   Dim rows As Range
+
+'    Set rows = Application.Workbooks("Point-of-Retirement-Reporting").Worksheets(1).Range("A2:A316")
+
+'    For Each cell In rows
+'        For Each innerCell In rows
+'            If cell.Value = innerCell.Value Then
+
+'        Next innerCell
+'    Next cell
+
+
+'End Sub
+
+
+
+
 Function parseDataString(inputData As String, delimiter As String) As String()
     Dim all As Variant, returnVal(2) As String
 
-    all = Split(inputData, delimiter)
+        all = Split(inputData, delimiter)
 
-    returnVal(0) = Trim(all(0))
-    returnVal(1) = Trim(all(1))
-    returnVal(2) = Trim(all(2))
+        returnVal(0) = Trim(all(0))
+        returnVal(1) = Trim(all(1))
+        returnVal(2) = Trim(all(2))
 
-    parseDataString = returnVal
+        parseDataString = returnVal
 
 End Function
 
